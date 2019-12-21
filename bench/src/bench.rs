14d574d0be451cd0e37d3597acce7b1fd925836c
codri <>
2019-12-21 22:36:25 +0200

    Fixed .gitignore and updated bench

--- bench/src/bench.rs
+++ bench/src/bench.rs
@@ -5,7 +5,7 @@ use xorfilter::*;
 fn bench_xor_contains(c: &mut Criterion) {
     let xor = Xor8::new((0..100_000_000).collect::<Vec<u64>>().as_ref());
 
-    c.bench_function("xor_contains", |b| b.iter(|| xor.contains(402_124)));
+    c.bench_function("xor_contains", |b| b.iter(|| xor.contains(57_402_124)));
 }
 
 criterion_group!(benches, bench_xor_contains);
